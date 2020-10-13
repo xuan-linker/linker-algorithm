7d250e97f27a72e51c918de27f612376721edf51
xuanlccc@gmail.com <xuanlccc@gmail.com>
2020-10-13 16:40:00 +0800

    Add SelectionSort

--- src/com/xlccc/sys/select/SelectionSort.java
+++ src/com/xlccc/sys/select/SelectionSort.java
@@ -1,6 +1,4 @@
-package com.xlccc.sys;
-
-import java.util.Arrays;
+package com.xlccc.sys.select;
 
 /**
  * @author Linker
@@ -11,13 +9,14 @@ public class SelectionSort {
     public SelectionSort() {
     }
 
-    public static void sort(int[] arr) {
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+
         // arr[0...i) 是有序的; arr[i...n) 是无序的
         for (int i = 0; i < arr.length; i++) {
             // 选择 arr[i...n) 中的最小值的索引
             int minIndex = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
@@ -25,18 +24,29 @@ public class SelectionSort {
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int t = arr[i];
+    private static <E> void swap(E[] arr, int i, int j) {
+        E t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 4, 2, 3, 6, 5};
+        Integer[] arr = {1, 4, 2, 3, 6, 5};
         SelectionSort.sort(arr);
         for (int e : arr
         ) {
             System.out.println(e + " ");
+        }
+        System.out.println();
+
+        Student[] students = {new Student("a", 1),
+                new Student("b", 2),
+                new Student("c", 3)};
+        SelectionSort.sort(students);
+        for (Student s : students
+        ) {
+            System.out.println(s);
+
         }
 //        System.out.println(Arrays.toString(arr));
     }
